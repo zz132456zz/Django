070cd4260e31664d7e246db76646acb27b6d5ca8
zz132456zz <zz132456zz@kakao.com>
2021-02-18 14:46:13 +0900

    Create a blog

---
+++ djangonautic/djangonautic/urls.py
@@ -0,0 +1,25 @@
+#from django.urls import path, include
+from django.conf.urls import url, include
+from django.contrib import admin
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+from articles import views as article_views
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+#     path('articles/', include('articles.urls')),
+#     path('about/', views.about),
+#     path('', views.homepage)
+# ]
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('accounts.urls')),
+    url(r'^articles/', include('articles.urls')),
+    url(r'^about/$', views.about),
+    url(r'^$', article_views.article_list, name="home"),
+]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
